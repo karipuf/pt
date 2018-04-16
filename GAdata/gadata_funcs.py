a35b0f3636b3a0ca80e34469abc2d21caf786eb9
Wei Lee Woon <wlwoon@gmail.com>
2018-04-16 14:31:10 +0400

    Just before moving to generalized feature mode

--- GAdata/gadata_funcs.py
+++ GAdata/gadata_funcs.py
@@ -86,3 +86,80 @@ def getTrainingData(featThresholds=150,featureWindow=30,labelWindow=20,nanReplac
             ydf=ydf_
         
     return xdf,ydf
+
+
+
+###############################################################################
+# Old/deprecated versions
+
+# Feature extraction
+def getTrainingData_old(featThresholds=150,featureWindow=30,labelWindow=20,nanReplace=-10):
+
+    if not isinstance(featThresholds,Iterable):
+        featThresholds=[featThresholds]
+
+    ################################
+    # Warning warning - OLD VERSION
+    ################################
+    
+    for featThreshold in featThresholds:
+
+        # Preparing to extract features
+        inFeatRange=set(list(range(featThreshold-featureWindow,featThreshold)))
+        labelRange=set(list(range(featThreshold,featThreshold+labelWindow)))
+    
+        featdf=df[df.day.isin(inFeatRange)]
+        targetdf=df2[df2.day.isin(labelRange)]
+        targetdf=targetdf[targetdf.customer_id.isin(set(featdf.user_id))]
+    
+        featdf=featdf.replace(np.nan,nanReplace)
+        
+        dg=featdf.groupby('user_id')
+
+
+        ################################
+        # Warning warning - OLD VERSION
+        ################################
+    
+        
+        # Feature extraction
+
+        # x (inputs) - xdf_ is the dataframe containing all possible features
+        # in next cell only interesting ones are extracted
+        xdf_=dg.hits.count()
+        xdf_=pd.concat((xdf_,dg.country.first()),axis=1)
+        xdf_['malaysia']=(xdf_.country=='Malaysia').astype(int)
+        xdf_['malsing']=(xdf_.country.isin(['Malaysia','Singapore'])).astype(int)
+        xdf_['meanDay']=dg.day.mean()-(featThreshold-featureWindow)
+        xdf_['stdDay']=dg.day.std().replace(np.nan,0)
+        xdf_['meanDuration']=dg.sessionDuration.mean()
+        xdf_['maxDuration']=dg.sessionDuration.max()
+        xdf_['meanPurchase']=dg.noPurchases.mean()
+        xdf_['maxPurchase']=dg.noPurchases.max()
+        xdf_['recency']=dg.day.apply(lambda tmp: np.min(featThreshold-tmp))
+        xdf_['userType']=(dg.userType.first()=='Returning Visitor').astype(int)
+        xdf_['bigCity']=(dg.city.first().isin(['Kuala Lumpur','Johor Bahru','Petaling Jaya'])).astype(int)
+        xdf_['city']=dg.city.first()
+        xdf_['bigRegion']=(dg.region.first().isin(['Federal Territory of Kuala Lumpur','Selangor','Johor','Penang']))
+        xdf_['region']=dg.region.first()
+        xdf_['maxSessions']=dg.sessions.max()
+        xdf_['meanSessions']=dg.sessions.mean()
+        
+        ################################
+        # Warning warning - OLD VERSION
+        ################################
+    
+        
+        # y (targets)
+        ydf_=pd.DataFrame(xdf_.index.isin(set(targetdf.customer_id)).astype(int),index=xdf_.index)
+
+        try:
+            xdf_=xdf_[np.logical_not(xdf_.index.isin(xdf.index))]
+            ydf_=ydf_[np.logical_not(ydf_.index.isin(xdf.index))]
+            xdf=pd.concat((xdf,xdf_),axis=0)
+            ydf=pd.concat((ydf,ydf_),axis=0)
+        except NameError:
+            xdf=xdf_
+            ydf=ydf_
+        
+    return xdf,ydf
