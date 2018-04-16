67fa0b8a617721fe4f204165e526af26e749fe5c
Wei Lee Woon <wlwoon@gmail.com>
2018-04-16 23:50:35 +0400

    Changed gadata_funcs so that it doesn't have to load the training data all the time

--- GAdata/gadata_funcs.py
+++ GAdata/gadata_funcs.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from collections import Iterable
+from os.path import join
 
 # Utility stuff
 ##################
@@ -9,34 +10,41 @@ from collections import Iterable
 monthDict={'01':0,'02':31,'03':59,'04':90,'05':120,'06':151,'07':181,'08':212,'09':243,'10':273,'11':304,'12':334} # Only for non-leap years OK
 intMonthDict={int(tmp):monthDict[tmp] for tmp in monthDict.keys()}
 
-# Functions
-date2days=lambda year,month,day: (int(year)-2017)*365+monthDict[month]+int(day)
-gadata_date2days=lambda datestr: date2days(datestr[:4],datestr[4:6],datestr[6:8]) 
-salesdata_date2days=lambda datestr: date2days(datestr[:4],datestr[5:7],datestr[8:10])
-
-
-# Loading in global data
-###############################
-
-# Loading in data and adding day info
-df=pd.read_csv("gadata.csv",dtype={'date':str})
-df2=pd.read_csv("../withSalesData/sales_order_line_timestamped.csv")
-df['day']=df.date.apply(gadata_date2days)
-df2['day']=df2.transaction_date.apply(salesdata_date2days)
 
 
 
 # Functions
 #############
 
-def extractFeaturesGA(featdf,featThreshold=-1,nanReplace=-10,featureWindow=38):
+date2days=lambda year,month,day: (int(year)-2017)*365+monthDict[month]+int(day)
+gadata_date2days=lambda datestr: date2days(datestr[:4],datestr[4:6],datestr[6:8]) 
+salesdata_date2days=lambda datestr: date2days(datestr[:4],datestr[5:7],datestr[8:10])
+
+
+def LoadGlobalData(gaPath='.',salesDataPath='../withSalesData'):
+    '''
+    Loading in data and adding day info
+
+    *Only required when training models*
+    ------------------------------------
+
+    '''
+
+    global df,df2
+    df=pd.read_csv(join(gaPath,"gadata.csv"),dtype={'date':str})
+    df2=pd.read_csv(join(salesDataPath,"sales_order_line_timestamped.csv"))
+    df['day']=df.date.apply(gadata_date2days)
+    df2['day']=df2.transaction_date.apply(salesdata_date2days)
+
+
+def ExtractFeaturesGA(featdf,featThreshold=-1,nanReplace=-10,featureWindow=26):
     '''
     Feature Extraction
     ------------------
     featdf is the window of Google Analytics events
     for which features are to be extracted
 
-    def extractFeaturesGA(featdf,featThreshold=-1,nanReplace=-10):
+    def ExtractFeaturesGA(featdf,featThreshold=-1,nanReplace=-10):
     featdf - dataframe containing relevant chunk of GA data (in the format extracted from gadata.csv" 
     featThreshold - the day (day 1 -> Jan 1 2017) which is considered "now"
     nanReplace - replace nans.. best not to tamper ;-)
@@ -80,7 +88,7 @@ def extractFeaturesGA(featdf,featThreshold=-1,nanReplace=-10,featureWindow=38):
     return xdf
 
 # Feature extraction
-def getTrainingData(featThresholds=[80,150],featureWindow=38,labelWindow=26,nanReplace=-10):
+def GetTrainingData(featThresholds=[80,150],featureWindow=26,labelWindow=38,nanReplace=-10):
 
     if not isinstance(featThresholds,Iterable):
         featThresholds=[featThresholds]
@@ -96,82 +104,7 @@ def getTrainingData(featThresholds=[80,150],featureWindow=38,labelWindow=26,nanR
         targetdf=df2[df2.day.isin(labelRange)]
         targetdf=targetdf[targetdf.customer_id.isin(set(featdf.user_id))]
 
-        xdf_=extractFeaturesGA(featdf,featThreshold,nanReplace)
-        ydf_=pd.DataFrame(xdf_.index.isin(set(targetdf.customer_id)).astype(int),index=xdf_.index)
-
-        try:
-            xdf_=xdf_[np.logical_not(xdf_.index.isin(xdf.index))]
-            ydf_=ydf_[np.logical_not(ydf_.index.isin(xdf.index))]
-            xdf=pd.concat((xdf,xdf_),axis=0)
-            ydf=pd.concat((ydf,ydf_),axis=0)
-        except NameError:
-            xdf=xdf_
-            ydf=ydf_
-        
-    return xdf,ydf
-
-###############################################################################
-# Old/deprecated versions
-
-# Feature extraction
-def getTrainingData_old(featThresholds=150,featureWindow=30,labelWindow=20,nanReplace=-10):
-
-    if not isinstance(featThresholds,Iterable):
-        featThresholds=[featThresholds]
-
-    ################################
-    # Warning warning - OLD VERSION
-    ################################
-    
-    for featThreshold in featThresholds:
-
-        # Preparing to extract features
-        inFeatRange=set(list(range(featThreshold-featureWindow,featThreshold)))
-        labelRange=set(list(range(featThreshold,featThreshold+labelWindow)))
-    
-        featdf=df[df.day.isin(inFeatRange)]
-        targetdf=df2[df2.day.isin(labelRange)]
-        targetdf=targetdf[targetdf.customer_id.isin(set(featdf.user_id))]
-    
-        featdf=featdf.replace(np.nan,nanReplace)
-        
-        dg=featdf.groupby('user_id')
-
-
-        ################################
-        # Warning warning - OLD VERSION
-        ################################
-    
-        
-        # Feature extraction
-
-        # x (inputs) - xdf_ is the dataframe containing all possible features
-        # in next cell only interesting ones are extracted
-        xdf_=dg.hits.count()
-        xdf_=pd.concat((xdf_,dg.country.first()),axis=1)
-        xdf_['malaysia']=(xdf_.country=='Malaysia').astype(int)
-        xdf_['malsing']=(xdf_.country.isin(['Malaysia','Singapore'])).astype(int)
-        xdf_['meanDay']=dg.day.mean()-(featThreshold-featureWindow)
-        xdf_['stdDay']=dg.day.std().replace(np.nan,0)
-        xdf_['meanDuration']=dg.sessionDuration.mean()
-        xdf_['maxDuration']=dg.sessionDuration.max()
-        xdf_['meanPurchase']=dg.noPurchases.mean()
-        xdf_['maxPurchase']=dg.noPurchases.max()
-        xdf_['recency']=dg.day.apply(lambda tmp: np.min(featThreshold-tmp))
-        xdf_['userType']=(dg.userType.first()=='Returning Visitor').astype(int)
-        xdf_['bigCity']=(dg.city.first().isin(['Kuala Lumpur','Johor Bahru','Petaling Jaya'])).astype(int)
-        xdf_['city']=dg.city.first()
-        xdf_['bigRegion']=(dg.region.first().isin(['Federal Territory of Kuala Lumpur','Selangor','Johor','Penang']))
-        xdf_['region']=dg.region.first()
-        xdf_['maxSessions']=dg.sessions.max()
-        xdf_['meanSessions']=dg.sessions.mean()
-        
-        ################################
-        # Warning warning - OLD VERSION
-        ################################
-    
-        
-        # y (targets)
+        xdf_=ExtractFeaturesGA(featdf,featThreshold,nanReplace)
         ydf_=pd.DataFrame(xdf_.index.isin(set(targetdf.customer_id)).astype(int),index=xdf_.index)
 
         try:
---
+++ GAdata/testOptimizeResult.py
@@ -0,0 +1,106 @@
+import gadata_funcs,pdb,re
+import pandas as pd
+import pylab as pl
+import numpy as np
+from itertools import count
+from sys import stdout
+from gadata_funcs import date2days,gadata_date2days,salesdata_date2days,GetTrainingData,LoadGlobalData
+from importlib import reload
+from sklearn.ensemble import RandomForestClassifier,GradientBoostingClassifier,BaggingClassifier,VotingClassifier
+from sklearn.model_selection import train_test_split,ParameterSampler
+from sklearn.neural_network import MLPClassifier
+from xgboost import XGBClassifier
+from sklearn.metrics import precision_score,recall_score,accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import VotingClassifier
+
+# Conducting the experiments
+################################
+
+# Parameters
+#nAugment=1
+#pAugment=.69
+#labelWindow=38
+#featureWindow=26
+LoadGlobalData()
+labelThreshold=[80,150]
+
+featureList=['hits','maxPurchase','meanPurchase']
+numCV=12
+c=count(1)
+
+    
+params={
+    'learningRate':0.006353,
+    'featureWindow':26,
+    'labelWindow':38,
+    'propAugment':1,
+    'nHidden':31
+}
+
+paramsVec=[params[tmp] for tmp in ['learningRate','featureWindow','labelWindow','propAugment','nHidden']]
+learningRate,featureWindow,labelWindow,propAugment,nHidden=paramsVec 
+    
+# Loading data
+xdf,ydf=GetTrainingData(labelThreshold,labelWindow=labelWindow,featureWindow=featureWindow)
+x=xdf[featureList].values
+y=ydf.values.reshape((-1,))
+    
+n1s=pl.sum(y) # Number of positive samples
+n0s=pl.sum(1-y) # Number of negative samples
+n=(propAugment*n0s)-n1s # Number of extra positive samples that we have to create
+if n>0:
+    if n<n1s:
+        nAugment=1
+        pAugment=n/n1s
+    else:
+        if n<(2*n1s):
+            nAugment=2
+            pAugment=n/(2*n1s)
+        else:
+            nAugment=3
+            pAugment=n/(3*n1s)
+else:
+    nAugment=0
+    pAugment=0
+
+print("Evaluating parameter set #"+str(next(c))+", propAugment,nAugment and pAugment is "+str((propAugment,nAugment,pAugment)))
+    
+scores=[]
+stdout.write("Cross validation round: ")
+stdout.flush()
+for count1 in range(numCV):
+        
+    stdout.write("#"+str(count1)+",")
+    #rf=MLPClassifier(hidden_layer_sizes=(nHidden,),learning_rate_init=learningRate)
+    rf=RandomForestClassifier(n_estimators=50,max_depth=6)
+    #rf=XGBClassifier(n_estimators=50,max_depth=8)
+    #rf=VotingClassifier((('rf',RandomForestClassifier(n_estimators=50,max_depth=6)),('mlp',MLPClassifier(hidden_layer_sizes=(nHidden,),learning_rate_init=learningRate)),('xgb',XGBClassifier(n_estimators=250,max_depth=6))))
+    
+    # Creating test,train split
+    xtrain,xtest,ytrain,ytest=train_test_split(x,y,test_size=.05)
+    
+    # Augmenting minority class
+    xAug=0
+    for count2 in range(nAugment):
+        minoritySamp=np.logical_and(ytrain==1,np.random.rand(len(ytrain))<pAugment)
+        try:
+            xAug=np.concatenate((xAug,xtrain[minoritySamp]),axis=0)
+            yAug=np.concatenate((yAug,np.ones(np.sum(minoritySamp))),axis=0)
+        except ValueError:
+            xAug=xtrain[minoritySamp]
+            yAug=np.ones(np.sum(minoritySamp))
+                
+    xtrain=np.concatenate((xtrain,xAug),axis=0)
+    ytrain=np.concatenate((ytrain,yAug),axis=0)
+    
+    rf.fit(xtrain,ytrain)
+    scores.append([accuracy_score(ytest,rf.predict(xtest)),precision_score(ytest,rf.predict(xtest)),recall_score(ytest,rf.predict(xtest))])
+
+    stdout.write('\n')
+    stdout.write(','.join([str(tmp) for tmp in paramsVec]+[str(tmp) for tmp in np.mean(scores,axis=0)])+'\n')
+    stdout.flush()
+    
+print()
+scores=pd.DataFrame(scores,columns=['Accuracy','Precision','Recall'])
+print(np.mean(scores))
