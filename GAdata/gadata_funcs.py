fdc88f375379e3b0602dc19ad77a8ea462784be3
Wei Lee Woon <wlwoon@gmail.com>
2018-04-16 19:09:42 +0400

    Completed switch to extractFeatures + getTrainingData version

--- GAdata/gadata_funcs.py
+++ GAdata/gadata_funcs.py
@@ -10,9 +10,9 @@ monthDict={'01':0,'02':31,'03':59,'04':90,'05':120,'06':151,'07':181,'08':212,'0
 intMonthDict={int(tmp):monthDict[tmp] for tmp in monthDict.keys()}
 
 # Functions
-date2days=lambda month,day: monthDict[month]+int(day)
-gadata_date2days=lambda datestr: date2days(datestr[4:6],datestr[6:8]) 
-salesdata_date2days=lambda datestr: date2days(datestr[5:7],datestr[8:10])
+date2days=lambda year,month,day: (int(year)-2017)*365+monthDict[month]+int(day)
+gadata_date2days=lambda datestr: date2days(datestr[:4],datestr[4:6],datestr[6:8]) 
+salesdata_date2days=lambda datestr: date2days(datestr[:4],datestr[5:7],datestr[8:10])
 
 
 # Loading in global data
@@ -29,8 +29,58 @@ df2['day']=df2.transaction_date.apply(salesdata_date2days)
 # Functions
 #############
 
+def extractFeaturesGA(featdf,featThreshold=-1,nanReplace=-10,featureWindow=38):
+    '''
+    Feature Extraction
+    ------------------
+    featdf is the window of Google Analytics events
+    for which features are to be extracted
+
+    def extractFeaturesGA(featdf,featThreshold=-1,nanReplace=-10):
+    featdf - dataframe containing relevant chunk of GA data (in the format extracted from gadata.csv" 
+    featThreshold - the day (day 1 -> Jan 1 2017) which is considered "now"
+    nanReplace - replace nans.. best not to tamper ;-)
+    '''
+
+    # Prepping the data
+    if featThreshold==-1:
+        featThreshold=featdf.day.max()
+
+    featdf=featdf.replace(np.nan,nanReplace)
+    try:
+        featdf=featdf[featdf['day']>featThreshold-featureWindow]
+    except:
+        featdf['day']=featdf.date.apply(gadata_date2days)
+        featdf=featdf[featdf['day']>featThreshold-featureWindow]
+        
+    dg=featdf.groupby('user_id')
+    
+    # Feature extraction
+    # x (inputs) - xdf_ is the dataframe containing all possible features
+    # in next cell only interesting ones are extracted
+    xdf=dg.hits.count()
+    xdf=pd.concat((xdf,dg.country.first()),axis=1)
+    xdf['malaysia']=(xdf.country=='Malaysia').astype(int)
+    xdf['malsing']=(xdf.country.isin(['Malaysia','Singapore'])).astype(int)
+    xdf['meanDay']=dg.day.mean() #-(featThreshold-featureWindow)
+    xdf['stdDay']=dg.day.std().replace(np.nan,0)
+    xdf['meanDuration']=dg.sessionDuration.mean()
+    xdf['maxDuration']=dg.sessionDuration.max()
+    xdf['meanPurchase']=dg.noPurchases.mean()
+    xdf['maxPurchase']=dg.noPurchases.max()
+    xdf['recency']=dg.day.apply(lambda tmp: np.min(featThreshold-tmp))
+    xdf['userType']=(dg.userType.first()=='Returning Visitor').astype(int)
+    xdf['bigCity']=(dg.city.first().isin(['Kuala Lumpur','Johor Bahru','Petaling Jaya'])).astype(int)
+    xdf['city']=dg.city.first()
+    xdf['bigRegion']=(dg.region.first().isin(['Federal Territory of Kuala Lumpur','Selangor','Johor','Penang']))
+    xdf['region']=dg.region.first()
+    xdf['maxSessions']=dg.sessions.max()
+    xdf['meanSessions']=dg.sessions.mean()
+
+    return xdf
+
 # Feature extraction
-def getTrainingData(featThresholds=150,featureWindow=30,labelWindow=20,nanReplace=-10):
+def getTrainingData(featThresholds=[80,150],featureWindow=38,labelWindow=26,nanReplace=-10):
 
     if not isinstance(featThresholds,Iterable):
         featThresholds=[featThresholds]
@@ -45,35 +95,8 @@ def getTrainingData(featThresholds=150,featureWindow=30,labelWindow=20,nanReplac
         featdf=df[df.day.isin(inFeatRange)]
         targetdf=df2[df2.day.isin(labelRange)]
         targetdf=targetdf[targetdf.customer_id.isin(set(featdf.user_id))]
-    
-        featdf=featdf.replace(np.nan,nanReplace)
-        
-        dg=featdf.groupby('user_id')
-    
-        # Feature extraction
 
-        # x (inputs) - xdf_ is the dataframe containing all possible features
-        # in next cell only interesting ones are extracted
-        xdf_=dg.hits.count()
-        xdf_=pd.concat((xdf_,dg.country.first()),axis=1)
-        xdf_['malaysia']=(xdf_.country=='Malaysia').astype(int)
-        xdf_['malsing']=(xdf_.country.isin(['Malaysia','Singapore'])).astype(int)
-        xdf_['meanDay']=dg.day.mean()-(featThreshold-featureWindow)
-        xdf_['stdDay']=dg.day.std().replace(np.nan,0)
-        xdf_['meanDuration']=dg.sessionDuration.mean()
-        xdf_['maxDuration']=dg.sessionDuration.max()
-        xdf_['meanPurchase']=dg.noPurchases.mean()
-        xdf_['maxPurchase']=dg.noPurchases.max()
-        xdf_['recency']=dg.day.apply(lambda tmp: np.min(featThreshold-tmp))
-        xdf_['userType']=(dg.userType.first()=='Returning Visitor').astype(int)
-        xdf_['bigCity']=(dg.city.first().isin(['Kuala Lumpur','Johor Bahru','Petaling Jaya'])).astype(int)
-        xdf_['city']=dg.city.first()
-        xdf_['bigRegion']=(dg.region.first().isin(['Federal Territory of Kuala Lumpur','Selangor','Johor','Penang']))
-        xdf_['region']=dg.region.first()
-        xdf_['maxSessions']=dg.sessions.max()
-        xdf_['meanSessions']=dg.sessions.mean()
-       
-        # y (targets)
+        xdf_=extractFeaturesGA(featdf,featThreshold,nanReplace)
         ydf_=pd.DataFrame(xdf_.index.isin(set(targetdf.customer_id)).astype(int),index=xdf_.index)
 
         try:
@@ -86,8 +109,6 @@ def getTrainingData(featThresholds=150,featureWindow=30,labelWindow=20,nanReplac
             ydf=ydf_
         
     return xdf,ydf
-
-
 
 ###############################################################################
 # Old/deprecated versions
